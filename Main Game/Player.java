5cb418ee49e4ca38f91753dfc964fbccfd3170bf
mightymosdan <danielangelestiu@gmail.com>
2018-02-06 15:15:58 -0700

    Updated Player.java
    Updated Player.java to included documentation in line with JavaDoc commenting style.
    
    -Daniel

--- Main Game/Player.java
+++ Main Game/Player.java
@@ -1,5 +1,13 @@
 import java.util.*;
 
+/**
+*Represents the player class in the game
+*This class creates the instance of the player that is assigned to the user
+*The player class will include the method for possibleMoves that determine
+*whether a move is allowed or not
+*This class also includes the card that is played
+*/
+
 public class Player {
 	
 	private LinkedList<String> possibleMoves;
@@ -7,13 +15,35 @@ public class Player {
 	private Scanner user_input;
 	private String card;
 	
+	/**
+	*The getPossibleMoves method is the getter for the possibleMoves
+	*variable after it has been set
+	*@return possibleMoves that is set b the setter method setPossibleMoves
+	*/
+	
 	public LinkedList<String> getPossibleMoves() {
 		return possibleMoves;
 	}
 	
+	/**
+	*The setPossibleMoves is a method that assogms the possibleMoves variable 
+	*to the hand parameter
+	*@param hand 	is from the Deck class and has the cards that are in the 
+	*		player's hand that are used to determine what the player's
+	*		possible moves are
+	*/
+	
 	public void setPossibleMoves(LinkedList<String> hand) {
 		possibleMoves = hand;
 	}
+	
+	/**
+	*The move method asks the player which card they choose to place on their
+	*board
+	*This method checks to see if the user input is possible with the hand they have
+	*@param player	is used to determine which player is making a move and what hand
+	*		the player has to figure out if their move is possible
+	*/
 	
 	public void move(int player) {
 		user_input = new Scanner(System.in);
@@ -30,6 +60,11 @@ public class Player {
 			}
 		}
 	}
+	
+	/**
+	*The method cardPlayed just returns the card that is played
+	*@return 	the card that is chosen to be placed on the board
+	*/
 	
 	public String cardPlayed() {
 		return card;
